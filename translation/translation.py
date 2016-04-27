02a0ff6819cd0eec0ede0b16bef44d10fc6f130f
SJohnson <simonbjohnson@gmail.com>
2016-04-27 17:28:57 +0100

    removed wording at bottom

--- translation/translation.py
+++ translation/translation.py
@@ -1,14 +1,34 @@
 import hxl
 import re
 
+def compileLinks(contents):
+    apattern =re.compile("\<a (.*?)\>")
+    results = apattern.findall(contents)
+    output = []
+    for a in results:
+        output.append('<a '+a+'>')
+    return output
+    
+def substituteLinks(contents):
+    contents = re.sub("\<a (.*?)\>","<a>",contents)
+    return contents
+
+def reinsertLinks(contents,links):
+    for link in links:
+        contents = re.sub('<a>',link,contents,1)
+    return contents
+
 def translate(page,output,data,tag):
     
     with open(page, 'r') as myfile:
         contents=myfile.read()
+    links = compileLinks(contents)
+    contents = substituteLinks(contents)
     for row in data.with_columns(['#meta+text+en',tag]).gen_raw(show_headers=False, show_tags=True):
         print row
         print row[0] in contents
         contents = contents.replace(row[0],row[1])
+    contents = reinsertLinks(contents,links)
     contents = re.sub('src="(?!http)','src="../',contents)
     contents = re.sub('href="(?!http)','href="../',contents)
     print contents
